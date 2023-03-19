bd7c96f88a29f5345b89b01b552c679b3be83efb
Jakub Skowron <jakubskowron676@gmail.com>
2023-03-19 15:40:08 +0100

    Sandbox for testing the library

--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -4,11 +4,17 @@ int main()
 {
     std::string format = "{level} - {message}";
 
-    slou::Logger logger("slou", "%X", true, true, "slou.log", format);
+    slou::Logger logger("slou", "%x", true, true);
 
-    logger.Log(logger.SUCCESS,  "Successfully successed!");
-    logger.Log(logger.INFO,     "Successfully info'ed!");
-    logger.Log(logger.WARNING,  "Successfully warned!");
-    logger.Log(logger.ERROR,    "Successfully error'ed!");
-    logger.Log(logger.CRITICAL, "Successfully critical'ed!");
+    //logger.Log(logger.SUCCESS,  "Successfully successed!");
+    //logger.Log(logger.INFO,     "Successfully info'ed!");
+    //logger.Log(logger.WARNING,  "Successfully warned!");
+    //logger.Log(logger.ERROR,    "Successfully error'ed!");
+    //logger.Log(logger.CRITICAL, "Successfully critical'ed!");
+
+    logger.Log(logger.INFO, "This is being logged onto the screen!");
+
+    logger.logToScreen = false;
+
+    logger.Log(logger.INFO, "This isn't being logged onto the screen!");
 }
